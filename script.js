ccf79f790f1936d9fcd43ccd4525c93576e70fed
OzScarlet <170483445+OzScarlet@users.noreply.github.com>
2024-09-06 09:21:48 +0800

    Add prompt to get user input choice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+let getHumanChoice = prompt("Choose between rock, paper, or scissors.");
+if (getHumanChoice === "rock") {
+    console.log("You have chosen rock");
+}
+else if (getHumanChoice === "paper") {
+    console.log("You have chosen paper");
+}
+else if (getHumanChoice === "scissors") {
+    console.log("You have chosen scissors");
+}
+else console.log("Invalid input");
